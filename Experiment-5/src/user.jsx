4fa48c85d7dcdb5dfb847bedeef4e7d7d3b468ae
Soumyadip-1 <onemystery.0o@gmail.com>
2026-04-24 12:01:46 +0530

    Add files via upload

---
+++ Experiment-5/src/user.jsx
@@ -0,0 +1,16 @@
+function User() {
+    return (
+      <div>
+        <h1>User name is Suryansh Shandilya</h1>
+        <h1>User name is Suryansh Shandilya</h1>
+        <h1>User name is Suryansh Shandilya</h1>
+        <h1>User name is Suryansh Shandilya</h1>
+        <h1>User name is Suryansh Shandilya</h1>
+        <h1>User name is Suryansh Shandilya</h1>
+        <h1>User name is Suryansh Shandilya</h1>
+      </div>
+    );
+    
+  }
+  
+  export default User;
